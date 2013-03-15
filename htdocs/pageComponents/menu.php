150e02dc82ef87b000474a457dfd55872476b546
patternpaul <pattern.paul@gmail.com>
2013-03-15 07:56:59 -0500

    Removing Facebook like box
    This was broken and slowed down page load.

--- htdocs/pageComponents/menu.php
+++ htdocs/pageComponents/menu.php
@@ -1,5 +1,4 @@
 
-
         <div class="span3 right-content">
           	<h3>Player Leaders</h3>
                 
@@ -151,21 +150,6 @@
 
           		</tbody>
           	</table>
-
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=262911533729483";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-like" data-href="http://www.facebook.com/apps/application.php?id=262911533729483" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div>
-
-
-
-
 
           </div>
 
