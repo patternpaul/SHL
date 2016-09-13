c6a457dd5ad49c9a398d979478b823818cf08c77
Paul Everton <pattern.paul@gmail.com>
2016-09-13 16:41:06 -0500

    install

--- local_setup/install.sh
+++ local_setup/install.sh
@@ -5,9 +5,13 @@ cp /vagrant/.env.example /vagrant/.env
 chown nginx:nginx -R /vagrant
 chown nginx:nginx -R /usr/share/nginx/html/prd
 setenforce Permissive
-supervisorctl stop artisan_job:*
 service nginx restart
 cd /vagrant
+composer install
+composer dumpautoload -o
 php artisan migrate
 php artisan consumedata
 php artisan refreshredis
+php artisan route:cache
+php artisan config:cache
+php artisan optimize --force
