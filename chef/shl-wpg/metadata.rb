6ff97b8f37b52bf76434a1999b0182bdf93099d2
Paul Everton <pattern.paul@gmail.com>
2016-09-15 19:02:49 -0500

    additional updates to chef config

--- chef/shl-wpg/metadata.rb
+++ chef/shl-wpg/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pattern.paul@gmail.com'
 license 'no license. Just givr'
 description 'Sets up box for shl-wpg'
 long_description 'Sets up box for shl-wpg'
-version '1.0.7'
+version '1.0.9'
 
 
 depends 'users'
